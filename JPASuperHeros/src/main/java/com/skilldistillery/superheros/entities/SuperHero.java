c17b9398ed73982bec9da0821dda7a75b311829a
Cody Comstock <mr.codysherwood@hotmail.com>
2023-09-15 18:18:47 -0500

    Stub SuperHero intity, test hibernate config

---
+++ JPASuperHeros/src/main/java/com/skilldistillery/superheros/entities/SuperHero.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.superheros.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "super_hero")
+public class SuperHero {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	public SuperHero() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("SuperHero [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
+	
+}
---
+++ JPASuperHeros/src/test/java/com/skilldistillery/superheros/entities/SuperHeroTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.superheros.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SuperHeroTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private SuperHero hero;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPASuperHeroes");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		hero = em.find(SuperHero.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+			em.close();
+			hero = null;
+		}
+	
+
+	@Test
+	void test() {
+		assertNotNull(hero);
+		assertEquals("Cody", hero.getName());
+	}
+
+}
